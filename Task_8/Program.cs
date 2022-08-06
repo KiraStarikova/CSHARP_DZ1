78d4872a9acc0698c5ed48f30c003a6d12a4e1cc
Kira <starkial@lenta.ru>
2022-08-06 16:36:10 +0500

    Task 8 чётные числа от N до 1

---
+++ Task_8/Program.cs
@@ -0,0 +1,11 @@
+﻿//  принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine ());
+int count = 1;
+while (count < N)
+{
+    N = N - 1;
+    int result = N % 2;
+if (result == 0)
+Console.Write(N + " ");
+}
