58c3d0c3c5e78b73f315faf84d05cab614d79fb2
Kira <starkial@lenta.ru>
2022-08-06 15:51:30 +0500

    Task 6 является ли число чётным

---
+++ Task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿// принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine ());
+int result = num % 2;
+if (result == 0)
+{
+Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число нечётное");
+}
