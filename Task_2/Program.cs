683a4ca2073e2d296a99aafb048486fb107aae2e
Kira <starkial@lenta.ru>
2022-08-06 14:52:28 +0500

    Task 2 наибольшее из двух чисел

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,9 @@
 ﻿// на вход принимает два числа и выдаёт, какое число большее
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите первое число ");
+int num = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine ());
+int max = num;
+if (num >= max) max = num;
+if (num2 > max) max = num2;
+Console.WriteLine($"Наибольшее число {max}");
